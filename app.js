105ecd60d5a9ca865859e513bdee3931282e808a
audrilli <audreymeret.lohmann@zhdk.ch>
2023-10-26 16:06:45 +0200

    Works.

--- app.js
+++ app.js
@@ -29,7 +29,10 @@ var numVerticalLines = ellipseHeight - 1; // Adjust as needed
 var horizontalSpacing = canvas.height / numHorizontalLines;
 var verticalSpacing = canvas.width / numVerticalLines;
 
-var svgElements = "";
+// var svgElements = "";
+// Array to store line data
+var lineData = [];
+
 
 function draw() {
   layoutGenerator();
@@ -37,98 +40,88 @@ function draw() {
 
 function layoutGenerator() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  lineData = []; // Clear the line data
   //Grid properties
   var numHorizontalLines = ellipseHeight - 1; // Adjust as needed
   var numVerticalLines = ellipseHeight - 1; // Adjust as needed
   var horizontalSpacing = canvas.height / numHorizontalLines;
   var verticalSpacing = canvas.width / numVerticalLines;
 
+  // Draw grid lines
   for (var x = verticalSpacing; x < canvas.width; x += verticalSpacing) {
     ctx.beginPath();
     ctx.moveTo(x, 0);
     ctx.lineTo(x, canvas.height);
     ctx.stroke();
-    
-  }
+    lineData.push({ x1: x, y1: 0, x2: x, y2: canvas.height });
+}
 
-  for (var y = horizontalSpacing; y < canvas.height; y += horizontalSpacing) {
+for (var y = horizontalSpacing; y < canvas.height; y += horizontalSpacing) {
     ctx.beginPath();
     ctx.moveTo(0, y);
     ctx.lineTo(canvas.width, y);
     ctx.stroke();
-    
-  }
+    lineData.push({ x1: 0, y1: y, x2: canvas.width, y2: y });
+}
 
   // Set line properties
   ctx.lineWidth = lineWidth;
   ctx.strokeStyle = lineColor;
 
-  // Begin a path and move to the starting point
   ctx.beginPath();
-  ctx.moveTo(middleX, 0);
+            ctx.moveTo(middleX, 0);
+            ctx.lineTo(middleX, canvas.height);
+            ctx.stroke();
+            lineData.push({ x1: middleX, y1: 0, x2: middleX, y2: canvas.height });
 
-  // Draw a line to the ending point
-  ctx.lineTo(middleX, canvas.height);
+            ctx.beginPath();
+            ctx.moveTo(0, middleY);
+            ctx.lineTo(canvas.width, middleY);
+            ctx.stroke();
+            lineData.push({ x1: 0, y1: middleY, x2: canvas.width, y2: middleY });
 
-  // Stroke the line to make it visible
-  ctx.stroke();
+            ctx.fillStyle = ellipseFillColor;
+            ctx.strokeStyle = ellipseStrokeColor;
+            ctx.lineWidth = 2;
+
+            ctx.beginPath();
+            ctx.ellipse(middleX, middleY, majorRadius, minorRadius, 0, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.stroke();
   
-
-  //line V1
-  ctx.beginPath();
-  ctx.moveTo(middleX, 0);
-  ctx.lineTo(middleX, canvas.height);
-  ctx.stroke();
-
-  ctx.beginPath();
-  ctx.moveTo(0, middleY);
-  ctx.lineTo(canvas.width, middleY);
-  ctx.stroke();
-
-  //Ellipse
-  ctx.fillStyle = ellipseColor;
-  ctx.beginPath();
-  ctx.ellipse(middleX, middleY, ellipseWidth, ellipseHeight, 0, 0, 2 * Math.PI);
-  ctx.stroke();
-  svgElements += `<ellipse cx="${middleX}" cy="${middleY}" rx="${majorRadius}" ry="${minorRadius}" fill="${lineColor}" stroke="${lineColor}" stroke-width="2" />\n`;
 
   //loging the radius
   console.log("ellipseHeight:", ellipseHeight);
   console.log("ellipseWidth:", ellipseWidth);
 }
-function exportToSVG() {var svgString = buildSVG(); // Get the SVG string with elements
+function exportToSVG() {
+  var svgString = buildSVG(); // Get the SVG string with elements
 
-// Create a unique file name with a timestamp
-var timestamp = new Date().toISOString().replace(/[:.]/g, "-");
-var fileName = 'drawing_' + timestamp + '.svg';
+            // Create a unique file name with a timestamp
+            var timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+            var fileName = 'drawing_' + timestamp + '.svg';
 
-// Create a download link for the SVG file
-var blob = new Blob([svgString], { type: 'image/svg+xml' });
-var url = URL.createObjectURL(blob);
-var a = document.createElement('a');
-a.href = url;
-a.download = fileName;
-a.click();
+            // Create a download link for the SVG file
+            var blob = new Blob([svgString], { type: 'image/svg+xml' });
+            var url = URL.createObjectURL(blob);
+            var a = document.createElement('a');
+            a.href = url;
+            a.download = fileName;
+            a.click();
 }
-function buildSVG() { // Create an SVG string representing the elements
+function buildSVG() {  // Create an SVG string representing the elements
   var svgString = '<svg xmlns="http://www.w3.org/2000/svg" width="' + canvas.width + '" height="' + canvas.height + '">\n';
 
   // Include grid lines as paths
-  var horizontalSpacing = canvas.height / numHorizontalLines;
-  var verticalSpacing = canvas.width / numVerticalLines;
-
-  for (var x = verticalSpacing; x < canvas.width; x += verticalSpacing) {
-      svgString += '<path d="M' + x + ' 0 V' + canvas.height + '" stroke="' + lineColor + '" stroke-width="' + lineWidth + '" />\n';
-  }
-
-  for (var y = horizontalSpacing; y < canvas.height; y += horizontalSpacing) {
-      svgString += '<path d="M0 ' + y + ' H' + canvas.width + '" stroke="' + lineColor + '" stroke-width="' + lineWidth + '" />\n';
+  for (var i = 0; i < lineData.length; i++) {
+      svgString += '<path d="M' + lineData[i].x1 + ' ' + lineData[i].y1 + ' L' + lineData[i].x2 + ' ' + lineData[i].y2 + '" stroke="' + lineColor + '" stroke-width="' + lineWidth + '" />\n';
   }
 
   // Include ellipse as a path
   svgString += '<path d="M' + middleX + ' ' + middleY + ' m-' + majorRadius + ',0 a' + majorRadius + ',' + minorRadius + ' 0 1,0 ' + 2 * majorRadius + ',0 a' + majorRadius + ',' + minorRadius + ' 0 1,0 -' + 2 * majorRadius + ',0" fill="' + ellipseFillColor + '" stroke="' + ellipseStrokeColor + '" stroke-width="2" />\n';
 
-  svgString += '</svg';
+  svgString += '</svg>';
 
   return svgString;
 }
@@ -136,6 +129,7 @@ function buildSVG() { // Create an SVG string representing the elements
 canvas.addEventListener("click", function () {
   ellipseWidth += 1; // Increase major radius by 5 pixels
   ellipseHeight += 1; // Increase minor radius by 5 pixels
+  draw();
   
 });
 document.getElementById("exportButton").addEventListener("click", exportToSVG);
