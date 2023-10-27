bb28ff1819e50c2a9cade21d49a07216b8d00f2b
audrilli <audreymeret.lohmann@zhdk.ch>
2023-10-27 13:43:45 +0200

    Circle and middle guide work

--- Script/layoutGeneratorScript.js
+++ Script/layoutGeneratorScript.js
@@ -1,6 +1,7 @@
 // Check if there is an active document
 if (app.documents.length > 0) {
   var doc = app.activeDocument;
+  var diameter = 10;
 
   // Loop through all the pages in the document
   for (var i = 0; i < doc.pages.length; i++) {
@@ -12,6 +13,15 @@ if (app.documents.length > 0) {
     // Add a guide at the middle of the page
     var guide = page.guides.add(undefined, undefined);
     guide.location = middleX;
+
+    // Calculate the middle of the page
+    var middleX = page.bounds[1] + (page.bounds[3] - page.bounds[1]) / 2;
+    var middleY = page.bounds[0] + (page.bounds[2] - page.bounds[0]) / 2;
+
+     // Add a circle to the middle of the page
+    var circle = page.ovals.add(undefined, undefined);
+    circle.geometricBounds = [middleY - diameter / 2, middleX - diameter / 2, middleY + diameter / 2, middleX + diameter / 2];
+
   }
 } else {
   alert("No open document found. Please open a document before running this script.");
