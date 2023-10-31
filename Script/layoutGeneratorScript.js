c87d7820f5a05c738ae93c38c74d940cffb1d303
audrilli <audreymeret.lohmann@zhdk.ch>
2023-10-31 09:11:43 +0100

    Grid works

--- Script/layoutGeneratorScript.js
+++ Script/layoutGeneratorScript.js
@@ -23,44 +23,32 @@ if (app.documents.length > 0) {
     diameter += 1;
 
     //horizontal guides
-    doc.pages
-      .item(i)
-      .guides.add({
-        location: middleY,
-        orientation: HorizontalOrVertical.horizontal,
-      });
-    doc.pages
-      .item(i)
-      .guides.add({
-        location: middleY - diameter / 2,
-        orientation: HorizontalOrVertical.horizontal,
-      });
-    doc.pages
-      .item(i)
-      .guides.add({
-        location: middleY + diameter / 2,
-        orientation: HorizontalOrVertical.horizontal,
-      });
+    doc.pages.item(i).guides.add({
+      location: middleY,
+      orientation: HorizontalOrVertical.horizontal,
+    });
+    doc.pages.item(i).guides.add({
+      location: middleY - diameter / 2,
+      orientation: HorizontalOrVertical.horizontal,
+    });
+    doc.pages.item(i).guides.add({
+      location: middleY + diameter / 2,
+      orientation: HorizontalOrVertical.horizontal,
+    });
 
     //vertical guides
-    doc.pages
-      .item(i)
-      .guides.add({
-        location: middleX,
-        orientation: HorizontalOrVertical.vertical,
-      });
-    doc.pages
-      .item(i)
-      .guides.add({
-        location: middleX - diameter / 2,
-        orientation: HorizontalOrVertical.vertical,
-      });
-    doc.pages
-      .item(i)
-      .guides.add({
-        location: middleX + diameter / 2,
-        orientation: HorizontalOrVertical.vertical,
-      });
+    doc.pages.item(i).guides.add({
+      location: middleX,
+      orientation: HorizontalOrVertical.vertical,
+    });
+    doc.pages.item(i).guides.add({
+      location: middleX - diameter / 2,
+      orientation: HorizontalOrVertical.vertical,
+    });
+    doc.pages.item(i).guides.add({
+      location: middleX + diameter / 2,
+      orientation: HorizontalOrVertical.vertical,
+    });
 
     // Grid
     MyGrid();
@@ -70,21 +58,46 @@ if (app.documents.length > 0) {
     "No open document found. Please open a document before running this script."
   );
 }
-function MyGrid(){
+function MyGrid() {
   //calculate the marigns of the page
   var gutterWidth = diameter;
 
   var leftMargin = page.marginPreferences.left;
-    var rightMargin = page.marginPreferences.right;
+  var rightMargin = page.marginPreferences.right;
 
-     // Calculate the number of guides based on the gutter width
-     var numberOfGuides = Math.floor((page.bounds[3] - leftMargin - rightMargin) / gutterWidth);
- // Calculate the starting position for the first guide
- var startX = page.bounds[1] + leftMargin + (gutterWidth / 2);
+  var topMargin = page.marginPreferences.top;
+  var bottomMargin = page.marginPreferences.bottom;
+  
 
- // Add vertical guides with the specified gutter
- for (var j = 0; j < numberOfGuides; j++) {
-   var guide = page.guides.add(undefined, undefined);
-   guide.location = startX + j * gutterWidth;
- }
+  // Calculate the number of guides based on the gutter width
+  var numberOfGuides = Math.floor(
+    (page.bounds[3] - leftMargin - rightMargin) / gutterWidth
+  );
+  var numHorizontalGuides = Math.floor(
+    (page.bounds[2] - topMargin - bottomMargin) / gutterWidth
+  );
+  // alert(numHorizontalGuides);
+  
+
+  // Calculate the starting position for the first guide
+  var startX = page.bounds[1] + leftMargin;
+  +(gutterWidth / 2);
+  var startY = page.bounds[0] + topMargin + gutterWidth / 2;
+  // alert(startY);
+
+  // Add vertical guides with the specified gutter
+  for (var j = 0; j < numberOfGuides; j++) {
+    doc.pages.item(i).guides.add({
+      location: startX + j * gutterWidth,
+      orientation: HorizontalOrVertical.vertical,
+    });
+  }
+  //Add horizontal guides with specified gutter
+  for (var k = 0; k < numHorizontalGuides; k++) {
+    doc.pages.item(i).guides.add({
+      location: startY + k * gutterWidth,
+      orientation: HorizontalOrVertical.horizontal,
+    });
+  }
 }
+
